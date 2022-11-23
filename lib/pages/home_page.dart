81eebca6208e27a36ca991c2d31e57dfd58d42f1
fritsmeister12 <mattiekill010@gmail.com>
2022-11-23 02:55:46 +0100

    some init msg

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:habfit/components/habit_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Bool to control habit completed
+  bool habitCompleted = false;
+
+  // Checkbox was tapped
+  void checkBoxTapped(bool? value) {
+    setState(() {
+      habitCompleted = value!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: ListView(children: [
+        HabitTile(
+          habitName: 'Ontbijten',
+          habitCompleted: habitCompleted,
+          onChanged: (value) => checkBoxTapped(value),
+        ),
+      ]),
+    );
+  }
+}
